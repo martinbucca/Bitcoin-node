81f1b2a51ad4ce1f897cb7bcc3444b9e6b9756a5
martinbucca <mbucca@fi.uba.ar>
2023-08-09 15:55:23 -0300

    network.rs

--- src/network.rs
+++ src/network.rs
@@ -9,25 +9,26 @@ use crate::{
     logwriter::log_writer::{write_in_log, LogSender},
 };
 
-/// Devuelve una lista de direcciones Ipv4 obtenidas de la DNS seed y de los nodos ingresados manualmente en el archivo de configuración
+/// Returns a list of Ipv4 addresses obtained from the DNS seed and the nodes manually entered 
+/// in the configuration file
 pub fn get_active_nodes_from_dns_seed(
     config: &Arc<Config>,
     log_sender: &LogSender,
 ) -> Result<Vec<Ipv4Addr>, NodeCustomErrors> {
     let mut node_ips = Vec::new();
     if config.connect_to_dns_nodes {
-        // si en el archivo de configuracion esta seteado que se conecte a los nodos de la dns seed
+        // If in the configuration file it is set that it connects to the nodes of the dns seed
         get_nodes_from_dns_seed(config, log_sender, &mut node_ips)?;
     }
     for custom_node in config.custom_nodes_ips.iter() {
-        // por cada nodo ingresado manualmente en el archivo de configuracion
+        // For each node manually entered in the configuration file
         let custom_node_ip = match custom_node.parse::<Ipv4Addr>() {
             Ok(ip) => ip,
             Err(err) => {
                 write_in_log(
                     &log_sender.error_log_sender,
                     format!(
-                        "Error al parsear la ip {} del nodo ingresado manualmente: {}. Debe ser del tipo Ipv4: xxx.x.x.x",
+                        "Error trying to parse the ip {} of the manually entered node: {}. It must be Ipv4 format: xxx.x.x.x",
                         custom_node,
                         err
                     )
@@ -41,7 +42,7 @@ pub fn get_active_nodes_from_dns_seed(
     Ok(node_ips)
 }
 
-/// Obtiene las direcciones de los nodos a partir de la DNS seed
+/// Gets the addresses of the nodes from the DNS seed
 fn get_nodes_from_dns_seed(
     config: &Arc<Config>,
     log_sender: &LogSender,
@@ -60,7 +61,7 @@ fn get_nodes_from_dns_seed(
     write_in_log(
         &log_sender.info_log_sender,
         format!(
-            "Se obtuvieron {} ips de la DNS: {:?}\n",
+            "{} ips get from DNS: {:?}\n",
             node_ips.len(),
             node_ips
         )
