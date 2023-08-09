5f7a34fcdd06020c7ecd97d8d1bf24124394c246
martinbucca <mbucca@fi.uba.ar>
2023-08-09 16:16:24 -0300

    compact_size_uint ready

--- src/compact_size_uint.rs
+++ src/compact_size_uint.rs
@@ -1,17 +1,18 @@
 #[derive(Clone, Debug, PartialEq)]
-/// Representa un entero de largo variable según se utiliza en el protocolo bitcoin.
+/// Represents a variable length integer as used in the bitcoin protocol.
 pub struct CompactSizeUint {
     bytes: Vec<u8>,
 }
 
 impl CompactSizeUint {
-    /// Crea el CompactSize según el número recibido
+    /// Creates the CompactSize according to the number received
     pub fn new(value: u128) -> Self {
         CompactSizeUint {
             bytes: Self::generate_compact_size_uint(value),
         }
     }
-    /// Genera los bytes del compact size segpun el número recibido.
+
+    /// Generates the bytes of the compact size according to the number received.
     fn generate_compact_size_uint(value: u128) -> Vec<u8> {
         if (253..=0xffff).contains(&value) {
             return Self::get_compact_size_uint(0xfd, 3, value);
@@ -25,11 +26,11 @@ impl CompactSizeUint {
         vec![value as u8]
     }
 
-    /// Devuelve el CompactSize en formato bytes
+    /// Returns the CompactSize in bytes format.
     pub fn value(&self) -> &Vec<u8> {
         &self.bytes
     }
-    /// Devuelve el CompactSize decodificado en formato u64
+    /// Returns the decoded CompactSize in u64 format.
     pub fn decoded_value(&self) -> u64 {
         let mut bytes: [u8; 8] = [0; 8];
         bytes[0] = self.bytes[0];
@@ -48,7 +49,7 @@ impl CompactSizeUint {
         u64::from_le_bytes(bytes)
     }
 
-    /// Arma la cadena de bytes del compact size, según los parámetros recibidos
+    /// Builds the bytes of the compact size, according to the parameters received.
     fn get_compact_size_uint(first_byte: u8, bytes_amount: u8, value: u128) -> Vec<u8> {
         let mut bytes: Vec<u8> = Vec::new();
         bytes.push(first_byte);
@@ -64,22 +65,22 @@ impl CompactSizeUint {
         bytes
     }
 
-    /// Serializa el CompactSize a bytes y los devuelve
+    /// Marshalls the CompactSize to bytes and returns them.
     pub fn marshalling(&self) -> Vec<u8> {
         let mut bytes = vec![];
         bytes.extend(self.value());
         bytes
     }
 
-    /// Deserializa el CompactSize según los bytes recibidos y lo devuelve.
-    /// Actualiza el offset.
+    /// Unmarshalls the CompactSize according to the bytes received and returns it.
+    /// Updates the offset.
     pub fn unmarshalling(
         bytes: &[u8],
         offset: &mut usize,
     ) -> Result<CompactSizeUint, &'static str> {
         if bytes.len() - (*offset) < 1 {
             return Err(
-                "Los bytes recibidos no corresponden a un CompactSizeUnit, el largo es menor a 1 byte",
+                "The received bytes are not enough to unmarshall the CompactSizeUint",
             );
         }
         let first_byte = bytes[*offset];
@@ -110,112 +111,107 @@ mod test {
     use crate::compact_size_uint::CompactSizeUint;
 
     #[test]
-    fn test_el_numero_200_se_representa_como_0x_c8() {
-        let valor: u128 = 200;
-        let valor_retornado: CompactSizeUint = CompactSizeUint::new(valor);
-        let valor_esperado: Vec<u8> = vec![0xC8];
-        assert_eq!(*valor_retornado.value(), valor_esperado);
+    fn test_number_200_is_represented_as_0x_c8() {
+        let value: u128 = 200;
+        let returned_value: CompactSizeUint = CompactSizeUint::new(value);
+        let expected_value: Vec<u8> = vec![0xC8];
+        assert_eq!(*returned_value.value(), expected_value);
     }
 
     #[test]
-    fn test_el_numero_505_se_representa_como_0x_fd_f9_01() {
-        let valor: u128 = 505;
-        let valor_retornado: CompactSizeUint = CompactSizeUint::new(valor);
-        let valor_esperado: Vec<u8> = vec![0xFD, 0xF9, 0x01];
-        assert_eq!(*valor_retornado.value(), valor_esperado);
+    fn test_number_505_is_represented_as_0x_fd_f9_01() {
+        let value: u128 = 505;
+        let returned_value: CompactSizeUint = CompactSizeUint::new(value);
+        let expected_value: Vec<u8> = vec![0xFD, 0xF9, 0x01];
+        assert_eq!(*returned_value.value(), expected_value);
     }
 
     #[test]
-    fn test_el_numero_100000_se_representa_como_0x_fe_a0_86_01_00() {
-        let valor: u128 = 100000;
-        let valor_retornado: CompactSizeUint = CompactSizeUint::new(valor);
-        let valor_esperado: Vec<u8> = vec![0xFE, 0xA0, 0x86, 0x01, 0x00];
-        assert_eq!(*valor_retornado.value(), valor_esperado);
+    fn test_number_100000_is_represented_as_0x_fe_a0_86_01_00() {
+        let value: u128 = 100000;
+        let returned_value: CompactSizeUint = CompactSizeUint::new(value);
+        let expected_value: Vec<u8> = vec![0xFE, 0xA0, 0x86, 0x01, 0x00];
+        assert_eq!(*returned_value.value(), expected_value);
     }
 
     #[test]
-    fn test_el_numero_5000000000_se_representa_como_0x_ff_00_f2_05_2a_01_00_00_00() {
-        let valor: u128 = 5000000000;
-        let valor_retornado: CompactSizeUint = CompactSizeUint::new(valor);
-        let valor_esperado: Vec<u8> = vec![0xFF, 0x00, 0xF2, 0x05, 0x2A, 0x01, 0x00, 0x00, 0x00];
-        assert_eq!(*valor_retornado.value(), valor_esperado);
+    fn test_number_5000000000_is_represented_as_0x_ff_00_f2_05_2a_01_00_00_00() {
+        let value: u128 = 5000000000;
+        let returned_value: CompactSizeUint = CompactSizeUint::new(value);
+        let expected_value: Vec<u8> = vec![0xFF, 0x00, 0xF2, 0x05, 0x2A, 0x01, 0x00, 0x00, 0x00];
+        assert_eq!(*returned_value.value(), expected_value);
     }
 
     #[test]
-    fn test_unmarshalling_de_compact_size_de_1_byte_se_realiza_correctamente(
-    ) -> Result<(), &'static str> {
-        let compact_size_serializado: Vec<u8> = vec![0x30];
+    fn test_unmarshalling_of_1_byte_compact_size_is_done_correctly() -> Result<(), &'static str> {
+        let serialized_compact_size: Vec<u8> = vec![0x30];
         let mut offset: usize = 0;
-        let compact_size_esperado: CompactSizeUint =
-            CompactSizeUint::unmarshalling(&compact_size_serializado, &mut offset)?;
-        assert_eq!(compact_size_esperado.bytes, compact_size_serializado);
+        let expected_compact_size: CompactSizeUint =
+            CompactSizeUint::unmarshalling(&serialized_compact_size, &mut offset)?;
+        assert_eq!(expected_compact_size.bytes, serialized_compact_size);
         Ok(())
     }
 
     #[test]
-    fn test_unmarshalling_de_compact_size_de_3_bytes_se_realiza_correctamente(
-    ) -> Result<(), &'static str> {
-        let compact_size_serializado: Vec<u8> = vec![0xfd, 0x30, 0x20];
+    fn test_unmarshalling_of_3_byte_compact_size_is_done_correctly() -> Result<(), &'static str> {
+        let serialized_compact_size: Vec<u8> = vec![0xfd, 0x30, 0x20];
         let mut offset: usize = 0;
-        let compact_size_esperado: CompactSizeUint =
-            CompactSizeUint::unmarshalling(&compact_size_serializado, &mut offset)?;
-        assert_eq!(compact_size_esperado.bytes, compact_size_serializado);
+        let expected_compact_size: CompactSizeUint =
+            CompactSizeUint::unmarshalling(&serialized_compact_size, &mut offset)?;
+        assert_eq!(expected_compact_size.bytes, serialized_compact_size);
         Ok(())
     }
 
     #[test]
-    fn test_unmarshalling_de_compact_size_de_5_bytes_se_realiza_correctamente(
-    ) -> Result<(), &'static str> {
-        let compact_size_serializado: Vec<u8> = vec![0xFE, 0xA0, 0x86, 0x01, 0x00];
+    fn test_unmarshalling_of_5_byte_compact_size_is_done_correctly() -> Result<(), &'static str> {
+        let serialized_compact_size: Vec<u8> = vec![0xFE, 0xA0, 0x86, 0x01, 0x00];
         let mut offset: usize = 0;
-        let compact_size_esperado: CompactSizeUint =
-            CompactSizeUint::unmarshalling(&compact_size_serializado, &mut offset)?;
-        assert_eq!(compact_size_esperado.bytes, compact_size_serializado);
+        let expected_compact_size: CompactSizeUint =
+            CompactSizeUint::unmarshalling(&serialized_compact_size, &mut offset)?;
+        assert_eq!(expected_compact_size.bytes, serialized_compact_size);
         Ok(())
     }
 
     #[test]
-    fn test_unmarshalling_de_compact_size_de_9_bytes_se_realiza_correctamente(
-    ) -> Result<(), &'static str> {
-        let compact_size_serializado: Vec<u8> =
+    fn test_unmarshalling_of_9_byte_compact_size_is_done_correctly() -> Result<(), &'static str> {
+        let serialized_compact_size: Vec<u8> =
             vec![0xFF, 0x00, 0xF2, 0x05, 0x2A, 0x01, 0x00, 0x00, 0x00];
         let mut offset: usize = 0;
-        let compact_size_esperado: CompactSizeUint =
-            CompactSizeUint::unmarshalling(&compact_size_serializado, &mut offset)?;
-        assert_eq!(compact_size_esperado.bytes, compact_size_serializado);
+        let expected_compact_size: CompactSizeUint =
+            CompactSizeUint::unmarshalling(&serialized_compact_size, &mut offset)?;
+        assert_eq!(expected_compact_size.bytes, serialized_compact_size);
         Ok(())
     }
 
     #[test]
-    fn test_compact_size_de_valor_0x_fd_f9_01_devuelve_505_al_decodificarse() {
+    fn test_compact_size_of_value_0x_fd_f9_01_returns_505_when_decoded() {
         let bytes: Vec<u8> = vec![0xfd, 0xf9, 0x01];
         let compact_size: CompactSizeUint = CompactSizeUint { bytes };
-        let valor_esperado: u64 = compact_size.decoded_value();
-        assert_eq!(valor_esperado, 505);
+        let expected_value: u64 = compact_size.decoded_value();
+        assert_eq!(expected_value, 505);
     }
 
     #[test]
-    fn test_compact_size_de_valor_0x_c8_devuelve_200_al_decodificarse() {
+    fn test_compact_size_of_value_0x_c8_returns_200_when_decoded() {
         let bytes: Vec<u8> = vec![0xc8];
         let compact_size: CompactSizeUint = CompactSizeUint { bytes };
-        let valor_esperado: u64 = compact_size.decoded_value();
-        assert_eq!(valor_esperado, 200);
+        let expected_value: u64 = compact_size.decoded_value();
+        assert_eq!(expected_value, 200);
     }
 
     #[test]
-    fn test_compact_size_de_valor_0x_fe_a0_86_01_00_devuelve_100000_al_decodificarse() {
+    fn test_compact_size_of_value_0x_fe_a0_86_01_00_returns_100000_when_decoded() {
         let bytes: Vec<u8> = vec![0xFE, 0xA0, 0x86, 0x01, 0x00];
         let compact_size: CompactSizeUint = CompactSizeUint { bytes };
-        let valor_esperado: u64 = compact_size.decoded_value();
-        assert_eq!(valor_esperado, 100000);
+        let expected_value: u64 = compact_size.decoded_value();
+        assert_eq!(expected_value, 100000);
     }
 
     #[test]
-    fn test_compact_size_de_valor_0x_ff_00_f2_05_2a_01_00_00_00_devuelve_5000000000_al_decodificarse(
-    ) {
+    fn test_compact_size_of_value_0x_ff_00_f2_05_2a_01_00_00_00_returns_5000000000_when_decoded() {
         let bytes: Vec<u8> = vec![0xFF, 0x00, 0xF2, 0x05, 0x2A, 0x01, 0x00, 0x00, 0x00];
         let compact_size: CompactSizeUint = CompactSizeUint { bytes };
-        let valor_esperado: u64 = compact_size.decoded_value();
-        assert_eq!(valor_esperado, 5000000000);
+        let expected_value: u64 = compact_size.decoded_value();
+        assert_eq!(expected_value, 5000000000);
     }
 }
