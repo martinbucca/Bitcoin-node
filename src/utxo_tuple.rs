51334a066b854560bd30b4af6886317e76b215fe
martinbucca <mbucca@fi.uba.ar>
2023-08-09 15:31:04 -0300

    utxo_tuple ready

--- src/utxo_tuple.rs
+++ src/utxo_tuple.rs
@@ -1,20 +1,22 @@
 use crate::transactions::tx_out::TxOut;
 
-/// Guarda el hash de la transacción y un array con los TxOut sin gastar, referentes a esa transacción
-/// La tupla guarda el TxOut y el indice en el que se encuentra en la tx
 #[derive(Debug, Clone)]
+/// Stores the hash of the transaction and an array with the unspent TxOut, referring to that transaction
+/// The tuple stores the TxOut and the index in which it is located in the tx
 pub struct UtxoTuple {
     pub hash: [u8; 32],
     pub utxo_set: Vec<(TxOut, usize)>,
 }
 
 impl UtxoTuple {
+
+    /// Creates a new UtxoTuple
     pub fn new(hash: [u8; 32], utxo_set: Vec<(TxOut, usize)>) -> Self {
         UtxoTuple { hash, utxo_set }
     }
 
-    /// Devuelve la utxoTuple con las TxOut que referencian a la dirección recibida
-    /// En caso de que no encuentre ninguna, devuelve None
+    /// Returns the utxoTuple with the TxOut that reference the received address
+    /// If it does not find any, returns None
     pub fn referenced_utxos(&self, address: &str) -> Option<UtxoTuple> {
         let hash = self.hash;
         let mut utxo_set: Vec<(TxOut, usize)> = Vec::new();
@@ -36,7 +38,7 @@ impl UtxoTuple {
         Some(UtxoTuple { hash, utxo_set })
     }
 
-    /// Devuelve el monto en satoshis de las TxOut del Utxo
+    /// Returns the amount in satoshis of the TxOut of the Utxo
     pub fn balance(&self) -> i64 {
         let mut balance = 0;
         for utxo in &self.utxo_set {
@@ -45,12 +47,14 @@ impl UtxoTuple {
         balance
     }
 
+    /// Returns the hash of the transaction
     pub fn hash(&self) -> [u8; 32] {
         self.hash
     }
-    /// Funcion que se usa para la generacion de los txIn a la hora de crear una nueva transaccion
-    /// puede suceder que una transaccion tenga mas de un outpoint referenciando a las utxos de esa
-    /// transaccion por eso la necesidad de esta funcion
+
+    /// It is used to generate the txIn when creating a new transaction
+    /// it can happen that a transaction has more than one outpoint referencing the utxos of that
+    /// transaction that is why the need for this function
     pub fn get_indexes_from_utxos(&self) -> Vec<usize> {
         let mut indexes = Vec::new();
         for utxo in &self.utxo_set {
@@ -59,16 +63,14 @@ impl UtxoTuple {
         indexes
     }
 
-    /// Recive el monto total a gastar, y monto que ya se juntó
-    /// Remueve las utxos necesarias hasta llegar al monto total y las devuelve en un nuevo UtxoTuple
+    /// Receives the total amount to spend, and the amount that has already been raised
+    /// Removes the necessary utxos until reaching the total amount and returns them in a new UtxoTuple
     pub fn utxos_to_spend(&mut self, value: i64, partial_amount: &mut i64) -> UtxoTuple {
         let mut utxos_to_spend = Vec::new();
         let mut position: usize = 0;
         let lenght: usize = self.utxo_set.len();
         while position < lenght {
             *partial_amount += self.utxo_set[position].0.value();
-            // No corresponde removerlas mientras la tx no está confirmada
-            // let utxo = self.utxo_set.remove(position);
             utxos_to_spend.push(self.utxo_set[position].clone());
             if *partial_amount > value {
                 break;
@@ -78,8 +80,8 @@ impl UtxoTuple {
         Self::new(self.hash, utxos_to_spend)
     }
 
-    /// Busca la utxo que corresponde al hash e indice recibido.
-    /// Devuelve su pub key script en formato bytes
+    /// Search the utxo that corresponds to the received hash and index.
+    /// Returns its pub key script in bytes format
     pub fn find(&self, previous_hash: [u8; 32], previous_index: usize) -> Option<&Vec<u8>> {
         if self.hash != previous_hash {
             return None;
@@ -92,7 +94,7 @@ impl UtxoTuple {
         None
     }
 
-    /// Remueve el output que contiene el indice recibido.
+    /// Removes the output that contains the received index.
     pub fn remove_utxo(&mut self, output_index: usize) {
         for index in 0..self.utxo_set.len() {
             if self.utxo_set[index].1 == output_index {
@@ -102,7 +104,3 @@ impl UtxoTuple {
         }
     }
 }
-
-#[cfg(test)]
-
-mod test {}
